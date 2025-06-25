f22da01ca210a655068d37ada510c408a848523f
Clrangercode <clranger23@gmail.com>
2025-06-25 07:37:09 -0300

    atualizei a pasta

---
+++ mensagem.js
@@ -0,0 +1,5 @@
+function carConteudo(){
+    document.getElementById("conteudo") 
+    .innerHTML =
+     "<p>Não deixe que a vida passe desapercebida, viaje e aproveite tudo de bom que a vida tem pra dar!</p>";
+}
